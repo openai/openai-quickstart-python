28578bc65fc2dbf38e620a5d041b1fabf3bfdd3b
dalilali <dalil.ali@gmx.de>
2023-01-15 01:22:38 +0100

    when Cron schedule excute ..

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
+"""os.getenv("OPENAI_API_KEY")"""
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        cronjob = request.form["cronjob"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(cronjob),
             temperature=0.6,
+            max_tokens=1024
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +24,13 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(cronjob):
+    return """Suggest the schedule time in the easiest way
+Cronjob: * * * * *
+schedule interval: every minute
+cronjob: 0 1 * * *
+schedule: every day at 01:00
+Cronjob: {}
+schedule:""".format(
+        cronjob.capitalize()
     )
