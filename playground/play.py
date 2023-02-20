2a4928a7f2d9460c9873e2b7626691dc8bf5ccb5
Scott Morgan <scott.morgan3142@gmail.com>
2023-02-20 12:32:46 +0000

    Adds folder structure to playground

---
+++ playground/play.py
@@ -0,0 +1,7 @@
+import os
+import openai
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
