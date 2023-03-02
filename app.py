a90793ecb8a515d486b6f5aea7eb07b946739103
Sourasis Roy <sourasis.roy@gmail.com>
2023-03-02 20:30:00 +0530

    added gpt-3.5-turbo support

--- app.py
+++ app.py
@@ -21,6 +21,28 @@ def index():
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
+# run on model gpt-3.5-turbo 
+@app.route("/turbo", methods=("GET", "POST"))
+def turbo():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.ChatCompletion.create( #Needs OpenAI 0.27 above )
+            model="gpt-3.5-turbo",
+            messages=[
+                        {"role": "system", "content": "You are a expert in zoology"},
+                        {"role": "assistant", "content": "Suggest three names for an animal that is a superhero."},
+                        {"role": "user", "content": "Cat"},
+                        {"role": "assistant", "content": "Captain Sharpclaw, Agent Fluffball, The Incredible Feline"},
+                        {"role": "user", "content": "Dog"},
+                        {"role": "assistant", "content": "Ruff the Protector, Wonder Canine, Sir Barks-a-Lot"},
+                        {"role": "user", "content": animal},
+                    ]
+        )
+        return redirect(url_for("turbo", result=response['choices'][0]['message']['content']))
+
+    result = request.args.get("result")
+    return render_template("index-turbo.html", result=result)
+
 
 def generate_prompt(animal):
     return """Suggest three names for an animal that is a superhero.
