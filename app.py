2f4a321cf497fe3dd1c9f16ed17e5c80cad9e0fd
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-03-26 19:06:12 +0100

    Updating app.py with faster code

--- app.py
+++ app.py
@@ -23,13 +23,11 @@ def index():
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return f"""Suggest three names for an animal that is a superhero.
 
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
 Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+Animal: {animal.capitalize()}
+Names: """
