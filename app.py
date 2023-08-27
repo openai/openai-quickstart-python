db88a45b9aea22a816a99028ae8ec69eae877a9b
Sanjeev Jagtap <sajagtap@microsoft.com>
2023-08-26 20:42:21 -0700

    Updated sample

--- app.py
+++ app.py
@@ -10,26 +10,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        store = request.form["store"]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=generate_messages(store),
+            temperature=0.6
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        return redirect(url_for("index", result=response['choices'][0]['message']['content']))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_messages(store):
+    return [{"role": "user", "content": "Suggest a cool name for the " + store}]
